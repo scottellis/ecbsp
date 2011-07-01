871fd6afdeba8b2f9e37756fb88808fd5a6e105b
Scott Ellis <scott@jumpnowtek.com>
2011-07-01 15:12:43 -0400

    Moved some defines where user-progs can see

--- ecbsp.h
+++ ecbsp.h
@@ -5,6 +5,22 @@
 #ifndef ECBSP_H
 #define ECBSP_H
 
+/*
+ The McBSP3 transmit buffer XB is 128×32 bit words.
+
+ To keep things simpler for our udelay calculation, lets restrict the 
+ number of motors to fit in XB in one copy.
+
+ This is not a hard limit and the DMA engine should handle the details,
+ but we'll start here until timing gets worked out.
+
+ 128 x 4 = 512 bytes is our max required DMA block size
+ 512 bytes x 4 motors per byte = 2048 max motors 
+*/
+#define DMA_BLOCK_SIZE 512
+#define MAX_MOTORS 2048
+
+
 /* ioctl commands */
 #define ECBSP_IOC_MAGIC		0xEC
 
